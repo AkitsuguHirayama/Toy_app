353810aa878383017b7b183162fca36d8f921845
AkitsuguHirayama <71302944+AkitsuguHirayama@users.noreply.github.com>
2021-05-29 09:09:28 +0900

    Add goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
     def hello
         render html:"hello,world!"
     end
+    
+    def goodbye
+        render html:"¡hola,goodbye!"
+    end
 end
